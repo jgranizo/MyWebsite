68b15ce2487cb411bcc43c237e79550bc2e0c8d2
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-01-10 15:14:05 -0500

    Added navigation bar
    Added links to my other components of my site displayed on the nav bar

--- MyWebsite/frontend/src/App.js
+++ MyWebsite/frontend/src/App.js
@@ -1,6 +1,18 @@
 import React ,{ Component,useState, useCallback } from 'react';
-import Plot from './components/charts/plot';
 import HomePage from './components/homepage';
+import Blog from './components/blog';
+import BlogsList from './components/blogs-list';
+import AddBlog from './components/add-blog';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+  Routes,
+  Route,
+  NavLink,
+  BrowserRouter,
+} from "react-router-dom";
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
 function App(){
   const[
     selectedChart,
@@ -18,14 +30,35 @@ function App(){
   return(
    
 <div>
+  <BrowserRouter>
+  <div>
+
+  <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="/">JGranizo</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to={"/"}>Home</Nav.Link>
+            <Nav.Link as={NavLink} to={"/blog"}>Blog</Nav.Link>
+            <Nav.Link as={NavLink} to={"/Resume"}>Resume</Nav.Link>
+            <Nav.Link as={NavLink} to={"/Projects"}>Projects</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+        <Routes>
+          <Route path="/" element={<HomePage></HomePage>}></Route>
+          <Route path="/blog" element={<Blog></Blog>}></Route>
+        </Routes>
+  </div>
   {/**<input type='radio'
   id="option1"
   value=""
   checked={selectedChart===""} onChange={()=>handleRadioChange("")}></input>
   
-  **/}<div> <HomePage></HomePage></div>
-  
+  **/} 
+  </BrowserRouter>
 </div>
+
   )
 }
 
---
+++ MyWebsite/frontend/src/components/blogs-list.js
@@ -0,0 +1,6 @@
+import React from "react";
+
+export default function BlogsList(){
+    return(<div>
+        </div>)
+}
