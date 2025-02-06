380540e68d8f61c92421c116859a598235cfb043
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-02-05 23:30:52 -0500

    Added resume page
    Added resume page and github,linkedin links, deleted unnecessary files

--- MyWebsite/frontend/src/App.js
+++ MyWebsite/frontend/src/App.js
@@ -2,7 +2,9 @@ import React ,{ Component,useState, useCallback } from 'react';
 import HomePage from './components/homepage';
 import Blog from './components/blog';
 import BlogsList from './components/blogs-list';
-import AddBlog from './components/add-blog';
+import Resume from './components/resume'
+import github from './components/images/github-mark.ico'
+import linkedin from './components/images/linkedin.ico'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
   Routes,
@@ -12,55 +14,43 @@ import {
 } from "react-router-dom";
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Login from './components/login';
 function App(){
-  const [user,setUser] = React.useState(null)
- const loginSetter = useCallback(user=>{
-  setUser(user);
+ 
 
- },[setUser]);
-
-async function logout(){
-  setUser(null)
-}
 
   return(
    
 <div>
   <div>
-
-  <Navbar bg="light" expand="lg">
+<div className='navbar'>
+  <Navbar bg="light" expand="md">
         <Navbar.Brand href="/">JGranizo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
             <Nav.Link as={NavLink} to={"/"}>Home</Nav.Link>
             <Nav.Link as={NavLink} to={"/blogs"}>Blog</Nav.Link>
-            <Nav.Link as={NavLink} to={"/Resume"}>Resume</Nav.Link>
-            <Nav.Link as={NavLink} to={"/Projects"}>Projects</Nav.Link>
-            <Nav.Link as={NavLink} to={user? "" :"/login"}>{user ? "Logout User" : "Login"}</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-    </Navbar>
-    <Routes>
+          <Nav.Link as={NavLink} to={"/resume"}>Resume</Nav.Link>
 
+       </Nav>
+
+    </Navbar>
+    <div className='logos'>
+    <a href="https://www.linkedin.com/in/jeremygranizo/"target="_blank" rel="noopener noreferrer"><img src={linkedin} alt='linkedin icon' style={{height:"30px" ,width:"30px"}}></img></a>
+    <a href="https://www.github.com/jgranizo/"target="_blank" rel="noopener noreferrer"><img src={github} alt='github icon' style={{height:"30px" ,width:"25px"}}></img></a>
+    #TODO mail to me and fix css of logos
+    </div>
+    </div>
+    <Routes>
 
           <Route path='/' element={<HomePage />} />
           <Route path="/home" element={<HomePage />} />
 
           <Route path="/blogs" element={<BlogsList />} />
-          <Route path="/blog/:id" element={<Blog user={user} />} />
-          <Route path="/login" element={<Login login={user} loginSetter={loginSetter}/>} />
-          <Route path="/blogs/add" element={<AddBlog user={user} />} />
+          <Route path="/blog/:id" element={<Blog />} />
+          <Route path='/resume' element={<Resume/>}/>
       
         </Routes>
   </div>
-  {/**<input type='radio'
-  id="option1"
-  value=""
-  checked={selectedChart===""} onChange={()=>handleRadioChange("")}></input>
   
-  **/} 
   </div>
 
   )
