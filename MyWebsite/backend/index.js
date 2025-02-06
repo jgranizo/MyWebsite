eadbc50c8385ce2e373987c54907d35e39ce9429
jag254 <93292292+jgranizo@users.noreply.github.com>
2025-02-06 00:42:21 -0500

    fixed for vercel deployment

--- MyWebsite/backend/index.js
+++ MyWebsite/backend/index.js
@@ -6,7 +6,6 @@ import BlogDAO from './dao/blogDAO.js';
 async function main(){
     console.log("Starting main function...");
     dotenv.config();
-    console.log("Environment variables loaded:", process.env.MYBLOG_DB_URI);
     
     const client = new mongodb.MongoClient(
         process.env.MYBLOG_DB_URI
@@ -14,15 +13,11 @@ async function main(){
     const port = process.env.PORT || 8000
     try{
         //connect to MongoDB cluster
-        console.log("Attempting to connect to MongoDB...");
 await client.connect();
-console.log("Connected to MongoDB");
 await NotesDAO.injectDB(client)
 
-console.log("connected to Collection: notes")
 await BlogDAO.injectDB(client)
 
-console.log("connected to Collection: notes")
         
         app.listen(port, ()=>{
             console.log('server is running on port:' + port);
--- MyWebsite/frontend/src/services/blogs.js
+++ MyWebsite/frontend/src/services/blogs.js
@@ -2,37 +2,37 @@ import axios from "axios";
 
 class BlogDataService{
     getAll(page=0){
-        return axios.get(`http://localhost:5000/api/v1/blogs?page=${page}`)
+        return axios.get(`http://localhost:5000/?page=${page}`)
     }
 
 
     get(id){
-        return axios.get(`http://localhost:5000/api/v1/blogs/id/${id}`)
+        return axios.get(`http://localhost:5000/id/${id}`)
     }
     find(query,by = "topic", page = 0){
         return axios.get(
-            `http://localhost:5000/api/v1/blogs?${by}=${query}&page=${page}`
+            `http://localhost:5000/?${by}=${query}&page=${page}`
         )
     }
 
     createBlog(data){
-        return axios.post(`http://localhost:5000/api/v1/blogs/update`, data)
+        return axios.post(`http://localhost:5000/update`, data)
 
     }
     updateBlog(data){
-        return axios.put(`http://localhost:5000/api/v1/blogs/update`,data)
+        return axios.put(`http://localhost:5000/update`,data)
 
     }
 
     deleteBlog(id){
         return axios.delete(
-            `http://localhost:5000/api/v1/blogs/update`,
+            `http://localhost:5000/update`,
             {data:{blogId:id}}
         )
     }
 
     getTopic(){
-        return axios.get("http://localhost:5000/api/v1/blogs/topic")
+        return axios.get("http://localhost:5000/topic")
     }
 
 }
